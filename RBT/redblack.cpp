0b7d9d1a66aa0964b9b325048635e224ec6390af
luishwoliveira2016 <luis.biri25@hotmail.com>
2017-11-16 16:00:17 -0200

    implementacao rbtree

--- RBT/redblack.cpp
+++ RBT/redblack.cpp
@@ -1,3 +1,9 @@
+/*
+	Implementação RedBlack Tree
+
+	Acadêmico Luis Henriqu Wasem de Oliveira
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,39 +20,69 @@ struct tree{
 	node *nil;
 };
 
-void inorder(node *r){
-	if(r){
-		inorder(r->left);
-		printf("%d",r->key);
-		inorder(r->right);
-	}else printf("bug\n");
+node* min(tree* T, node* no){
+	node* aux = no;
+	while(aux->left != T->nil)
+		aux = aux->left;
+	return aux;
 }
 
-void preorder(node *r){
-	if(r){
-		
+node* sucessor(tree* T,node* n){
+	if(n->right != T->nil)
+		return min(T,n->right);
+	node* aux= n->parent;
+	while(aux != T->nil && n == aux->right){
+		n = aux;
+		aux = aux->parent;
+	}
+	return aux;
+}
+
+void inorder(node *r,node *nil){
+	if(r!=nil){
+		inorder(r->left,nil);
 		printf("%d",r->key);
-		preorder(r->left);
-		preorder(r->right);
+		printf("%c\n",r->color);
+		inorder(r->right,nil);
+	}	
+}
+
+void preorder(node *r,node* nil){
+	if(r!=nil){
+		printf("%d",r->key);
+		printf("%c\n",r->color);
+		preorder(r->left,nil);
+		preorder(r->right,nil);
 	}
 }
 
-void posorder(node *r){
-	if(r){
-		posorder(r->left);
-		posorder(r->right);
+void posorder(node *r,node *nil){
+	if(r!=nil){
+		posorder(r->left,nil);
+		posorder(r->right,nil);
 		printf("%d",r->key);
+		printf("%c\n",r->color);
 	}
 }
-
+//inicialização da arvore
 tree * start_tree(){
 	tree * n = (tree * )malloc(sizeof(tree));
 	n->nil = (node *)malloc(sizeof(node));
 	n->nil->color = 'B';
+	n->nil->parent = NULL;
+	n->nil->left = NULL;
+	n->nil->right = NULL;
 	n->root = n->nil;
+	
 	return n;
 }
-
+//busca nodo
+node * search_node(tree* T, node * r, int key){
+	if(r == T->nil || r->key == key ) return r;				
+	if(key <= r->key) return search_node(T,r->left, key);
+	return search_node(T,r->right, key);
+}
+//rotação a esquerda
 void leftRotate(tree *T , node * z){
 	node * y = z->right;
 
@@ -66,7 +102,7 @@ void leftRotate(tree *T , node * z){
 	y->left = z;
 	z->parent = y;
 }
-
+//rotação a direita
 void rightRotate(tree * T, node * z){
 	node * y = z->left;
 	
@@ -88,11 +124,10 @@ void rightRotate(tree * T, node * z){
 }
 
 
+// arrumar arvore ao inserir novo nodo
+node * fixUp_insert(tree * T, node * z){
 
-node * fixUp(tree * T, node * z){
-	
-	 	
-	while(z != T->root && z->parent->color == 'R'){
+	while( z->parent->color == 'R'){
 		if (z ->parent == z->parent->parent->left){
 			node * y = z->parent->parent->right;
 			if(y->color == 'R'){
@@ -104,10 +139,14 @@ node * fixUp(tree * T, node * z){
 				if(z == z->parent->right){
 					z = z->parent;
 					leftRotate(T,z);
+			
+					z->parent->color = 'B';
+					z->parent->parent->color = 'R';
+					rightRotate(T,z->parent->parent); 
+				}else{
+					z = z->parent;
+					rightRotate(T,z);
 				}
-				z->parent->color = 'B';
-				z->parent->parent->color = 'R';
-				rightRotate(T,z->parent->parent); 
 			}	
 		}else{
 			node *t = z->parent->parent->left;
@@ -120,16 +159,21 @@ node * fixUp(tree * T, node * z){
 				if(z == z->parent->left){
 					z = z->parent;
 					rightRotate(T,z);
+					z->parent->color = 'B';
+					z->parent->parent->color = 'R';
+					leftRotate(T,z->parent->parent); 
+				}else{
+					z = z->parent;
+					leftRotate(T,z);
 				}
-				z->parent->color = 'B';
-				z->parent->parent->color = 'R';
-				leftRotate(T,z->parent->parent); 
-			}
+			
 		}
+	}
+		
 	}
 	T->root->color = 'B';
 }
-
+// desenhar a  árvore (meritos ao colega)
 void drawTree(tree * t, node * n, int h){
 	if(n->left != t->nil)
 		drawTree(t, n->left, h+1);	
@@ -140,14 +184,14 @@ void drawTree(tree * t, node * n, int h){
 	}
 	
 	if(n->color == 'R')
-		printf("R-%d\n", n->key);	
+		printf("R-%d\n", n->key);
 	else
 		printf("B-%d\n", n->key);		
 	
 	if(n->right != t->nil)
 		drawTree(t, n->right, h+1);	
 }
-
+//inserção de um novo nodo
 void insert(tree *T, int z){
 
 		node * aux =(node *)malloc (sizeof(node));
@@ -180,36 +224,139 @@ void insert(tree *T, int z){
 			y->right = aux;
 		}
 		
-		fixUp(T,aux);
+		fixUp_insert(T,aux);
+}
+//arrumar a arvore apos deleção
+void fix_up_delecao(tree* T,node* x){
+	node* aux;
+
+	while(x != T->root && x->color == 'B'){
+		if(x == x->parent->left){
+			aux = x->parent->right;
+			if(aux->color == 'R'){
+				x->parent->color = 'R';
+				aux->color = 'B';
+				leftRotate(T,x->parent);
+				aux = x->parent->right;
+			}
+			if(aux->left->color == 'B' && aux->right->color == 'B'){
+				aux->color = 'R';
+				x = x->parent;
+				}else if(aux->right->color == 'B'){
+					aux->left->color = 'B';
+					aux->color = 'R';
+					rightRotate(T,aux);
+					aux = x->parent->right;
+				}else{
+				aux->color = aux->parent->color;
+				x->parent->color = 'B';
+				aux->right->color = 'B';
+				leftRotate(T,x->parent);
+				x = T->root;
+			}
+		}else{
+			aux = x->parent->left;
+			if(aux->color == 'R'){
+				aux->color = 'B';
+				x->parent->color = 'R';
+				rightRotate(T,x->parent);
+				aux = x->parent->left;
+			}
+			if(aux->right->color == 'B' && aux->left->color == 'B'){
+				aux->color = 'R';
+				x = x->parent;
+				}else if(aux->left->color == 'B'){
+					aux->right->color = 'B';
+					aux->color = 'R';
+					leftRotate(T,aux);
+					aux = x->parent->left;
+				}else{
+				aux->color= aux->parent->color;
+				x->parent->color = 'B';
+				aux->left->color = 'B';
+				rightRotate(T,x->parent);
+				x = T->root;
+			}
+		}
+	}
+	x->color = 'B';
 }
 
-int main(void){
+//deletar nodo da arvore
+node* del_node(tree* T, int key){
+	node* z = search_node(T, T->root, key);
+	node* y ,* x;
+	if(z == T->nil){
+		return z;
+	}
+	if(z->left == T->nil || z->right == T->nil)
+		y = z;
+		else
+			y = sucessor(T,z);
+	if(y->left != T->nil)
+		x = y->left;
+		else
+			x = y->right;
+	x->parent = y->parent;
+	if(y->parent == T->nil)
+		T->root = x;
+		else if(y == y->parent->left)
+			y->parent->left = x;
+		else
+			y->parent->right = x;
+	if(y != z)
+		z->key = y->key;
+	if(y->color == 'B')
+		fix_up_delecao(T,x);
+	return z;
+}
+
+
+
+int main(void){	
 	int opc,key;
 
 	tree * T = start_tree();
 
 	while(opc != 0 ){
-		printf("1-Inserir\n2-Deletar\n3-Buscar\n4-inorder\n5-posorder\n6-preorder\n");
+		printf("1-Inserir\n2-Deletar\n3-Buscar\n4-inorder\n5-posorder\n6-preorder\n7-desenhar\n0-sair:  ");
 		scanf("%d",&opc);
 
 		switch(opc){
+			case 0 :
+				return 0;
 			case 1 : 
 				printf("Insira a chave : \n");
 				scanf("%d",&key);
 				insert(T,key);
+
 				break;
 			case 2 :
-				printf("deletar\n");
+				printf("Insira o a cahve que deseja deletar:\n");
+				scanf("%d",&key);
+				del_node(T,key);
 				break;
 			case 3 : 
-				printf("buscar\n");
+				printf("insira a chave que deseja buscar:\n");
+				scanf("%d",&key);
+				if(search_node(T,T->root,key)!= T->nil){
+					printf("encontrou\n");
+				}else{
+					printf("o nodo nao esta na arvore\n");
+				}
 				break;
 			case 4 : 
-				inorder(T->root);
+				inorder(T->root,T->nil);
 				break;
+			case 5:
+				posorder(T->root,T->nil);
+				break;
+			case 6:
+				preorder(T->root,T->nil);
+				break;
+			case 7 :
+				drawTree(T,T->root,4);
 		} 	
 	}
-
 	return 0;
-
 }
